70f6217719c20608a8f598fefdc3bd230a1d3d86
Mariia Ufimtseva <mariia.ufimtseva@uni-weimar.de>
2022-11-08 20:26:45 +0100

    fixed speed of the bullet

--- app/src/main/java/de/buw/se4de/Bullet.java
+++ app/src/main/java/de/buw/se4de/Bullet.java
@@ -5,10 +5,17 @@ import java.awt.*;
 public class Bullet extends Object {
     Location location; // TODO: Use Location class
 
-    public Bullet(int x, int y, ID id,int dir_x, int dir_y) {
+    public Bullet(int x, int y, ID id, int dir_x, int dir_y) {
         super(x, y, id);
-        speed_x = (dir_x - x) / 10.0f;
-        speed_y = (dir_y - y) / 10.0f;
+        calculateSpeed(x, y, dir_x, dir_y);
+    }
+
+    public void calculateSpeed(int fromX, int fromY, int toX, int toY)
+    {
+        double distance = Math.sqrt(Math.pow((toX - fromX), 2) + Math.pow((toY - fromY), 2));
+        double speed = 10;
+        this.speed_y = (float)((toY - fromY) * speed / distance);
+        this.speed_x = (float)((toX - fromX) * speed / distance);
     }
 
     public void tick() {
@@ -18,7 +25,7 @@ public class Bullet extends Object {
     }
 
     public void draw(Graphics g) {
-        g.setColor(Color.blue);
-        g.fillOval(x, y, 5, 5);
+        g.setColor(Color.white);
+        g.fillOval(x, y, 10, 10);
     }
 }
