802af70f536566943f44d68bf2ac55e1e264dbd2
danie <daniel.ocks@uni-weimar.de>
2022-12-07 22:57:05 +0100

    Erste Tests zu Konstruktoren und Funktionen.

--- app/src/test/java/de/buw/se4de/entity/FireTest.java
+++ app/src/test/java/de/buw/se4de/entity/FireTest.java
@@ -21,5 +21,11 @@ class FireTest {
         assertEquals(4, fire.movementspeed);
         assertEquals(2, fire.health);
     }
+    @Test
+    void testConstructor2(){
+        Fire fire = new Fire(10, 10, new Handler());
+        fire.casttime = 0.0f;
+        fire.attack();
+    }
 
 }
